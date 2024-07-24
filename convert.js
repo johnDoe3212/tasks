9de2874f5914d81159184ea3eedc6c98134d609e
johnDoe3212 <99414911+johnDoe3212@users.noreply.github.com>
2024-07-24 14:43:38 +0200

    Update convert.js

--- convert.js
+++ convert.js
@@ -1,22 +1,11 @@
-function numToStr(num) {
-  return num.toString();
-}
-
-function stringToNumber(str) {
-  
-  if (typeof str === 'bigint') {
-    return num.toLocaleString('en-US');
-  } else if (typeof str === 'number') {
-    return num.toLocaleString('en-US', { minimumFractionDigits: 0, maximumFractionDigits: 20 });
-  } else if (typeof str === 'string') {
+function numToStrToBigInt(num) {
+  let str = num.toString();  
+  let [int, rest] = str.split(',');
+  let crazyBigInt = BigInt(int).toLocaleString('en-US');
     
-    let [int, rest] = str.split(',');
-    let crazyBigInt = BigInt(int).toLocaleString('en-US');
-    
-    if (rest) {
-      rest = rest.substring(0, 2);
-    }
-    
-    return `${crazyBigInt}.${rest}`
+  if (rest) {
+    rest = rest.substring(0, 2);
   }
+    
+  return `${crazyBigInt}.${rest}`
 }
